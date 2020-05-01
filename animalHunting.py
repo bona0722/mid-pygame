dec5d935b01775a55181d6dde5f5159ec3c6324c
koggk1 <koggk@naver.com>
2020-05-01 20:43:54 +0900

    animal hunting

--- animalHunting.py
+++ animalHunting.py
@@ -1,10 +1,39 @@
-import sys, pygame
+import sys, pygame, pygame.mixer
+import os
+from pygame.locals import QUIT
 
-pygame.init() #초기화를 해줘야함.
+pygame.init()
 
-pygame.display.set_caption("Window size 400") #게임 제목을 써줌. 화면이 꺼지기 전까지 제목이 계속 유지되므로 전역변수로 설정하기
-screen = pygame.display.set_mode((1000,700)) #x축,y축 생성
- # 화면을 초기화하거나 화면에 데이터 추가하는 변수
-start_backg = pygame.image.load(os.path.join("image", "Hunting_background.jpg")).convert()
+clock = pygame.time.Clock()
 
-for event in 
+size = width, height = 600, 400
+white = 255,255,255
+black = 0,0,0
+red = 255,0,0
+
+screen = pygame.display.set_mode(size)
+
+
+screen.fill(black)
+
+crashed = False
+
+
+while not crashed:
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            crashed = True
+
+        print(event)
+
+    
+    mx,my = pygame.mouse.get_pos()
+    screen.fill(black)
+    pygame.draw.line(screen, white, [mx-5, my],[mx-5, my] 20)
+    
+    pygame.display.update()
+    clock.tick(60)
+
+pygame.quit()
+quit
